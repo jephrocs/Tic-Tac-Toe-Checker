79612005a5b79312609c26d69cd0e3eeac41c018
Jeff C <50182734+jephrocs@users.noreply.github.com>
2020-08-24 01:34:06 -0700

    added solution

---
+++ solution.js
@@ -0,0 +1,72 @@
+function isSolved(board) {
+
+    //top left across
+    if (board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] === 1) {
+        return 1
+    }
+    if (board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] === 2) {
+        return 2
+    }
+    // mid across
+    if (board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] === 1) {
+        return 1
+    }
+    if (board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] === 2) {
+        return 2
+    }
+    // bottom left across
+    if (board[2][0] === board[2][1] && board[2][0] === board[2][2] && board[2][0] === 1) {
+        return 1
+    }
+    if (board[2][0] === board[2][1] && board[2][0] === board[2][2] && board[2][0] === 2) {
+        return 2
+    }
+    //top left down
+    if (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] === 1) {
+        return 1
+    }
+    if (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] === 2) {
+        return 2
+    }
+    //top mid down
+    if (board[0][1] === board[1][1] && board[0][1] === board[2][1] && board[0][1] === 1) {
+        return 1
+    }
+    if (board[0][1] === board[1][1] && board[0][1] === board[2][1] && board[0][1] === 2) {
+        return 2
+    }
+    // top right down
+    if (board[0][2] === board[1][2] && board[0][2] === board[2][2] && board[0][2] === 1) {
+        return 1
+    }
+    if (board[0][2] === board[1][2] && board[0][2] === board[2][2] && board[0][2] === 2) {
+        return 2
+    }
+    // top right diag
+    if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] === 1) {
+        return 1
+    }
+    if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] === 2) {
+        return 2
+    }
+    //bottom right diag
+    if (board[2][0] === board[1][1] && board[2][0] === board[0][2] && board[2][0] === 1) {
+        return 1
+    }
+    if (board[2][0] === board[1][1] && board[2][0] === board[0][2] && board[2][0] === 2) {
+        return 2
+    }
+    else for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j++) {
+            if (board[i][j] === 0) {
+                return -1
+            } else if (board[0][1] === 0) {
+                return -1
+            } else if (board[2][2] === 0) {
+                return -1
+            } return 0
+        }
+    }
+
+
+}
